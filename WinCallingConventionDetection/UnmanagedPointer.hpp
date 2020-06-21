1629ad3381a51659b60c1051dd6c987e64478055
Anthony <12345787+BreakShoot@users.noreply.github.com>
2020-06-21 03:55:49 -0400

    Added optimized returncheck bypass

--- WinCallingConventionDetection/UnmanagedPointer.hpp
+++ WinCallingConventionDetection/UnmanagedPointer.hpp
@@ -54,6 +54,7 @@ public:
 		this->m_Address = dwAddress;
 		auto* ccDetector = new CallingConventionDetector(this->m_Address, dwBaseAddress, true);
 		this->m_CallingConvention = ccDetector->GetCallingConvention();
+		this->RemoveReturnCheck();
 		ccDetector->PrintCallingConvention();
 		delete ccDetector;
 	}
@@ -64,6 +65,7 @@ public:
 		auto* ccDetector = new CallingConventionDetector(this->m_Address, dwBaseAddress, true);
 		this->m_CallingConvention = ccDetector->GetCallingConvention();
 		ccDetector->PrintCallingConvention();
+		this->RemoveReturnCheck();
 		delete ccDetector;
 	}
 
@@ -85,5 +87,47 @@ private:
 			if (DataCompare(reinterpret_cast<unsigned char*>(dwBaseAddress + i), (unsigned char*)(bMask), szMask))
 				return static_cast<uint32_t>(dwBaseAddress + i);
 		return 0;
+	}
+
+	void RemoveReturnCheck()
+	{
+		LPVOID   lpAllocation	= nullptr;
+		size_t   szFunctionSize = 0;
+		bool	 bRetcheck		= false;
+
+		do
+		{
+			szFunctionSize += 0x10;
+		} while (*reinterpret_cast<short*>(this->m_Address + szFunctionSize) != -29867 && *reinterpret_cast<BYTE*>(this->m_Address + szFunctionSize + 3) != 0xEC);
+
+
+		lpAllocation = VirtualAlloc(NULL, szFunctionSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+		memcpy(lpAllocation, reinterpret_cast<void const*>(this->m_Address), szFunctionSize);
+
+		for (size_t i = 0; i < szFunctionSize; ++i)
+		{
+			const uint32_t uiRobloxAdr = this->m_Address + i;
+			const uint32_t uiAllocAdr = reinterpret_cast<uint32_t>(lpAllocation) + i;
+
+			if (*reinterpret_cast<BYTE*>(uiAllocAdr) == 0xE8)
+			{
+				*reinterpret_cast<uint32_t*>(uiAllocAdr + 1) = (uiRobloxAdr + *reinterpret_cast<uint32_t*>(uiRobloxAdr + 1) + 5) - uiAllocAdr - 5;
+				i += 4; //don't scan rel32
+			}
+
+			if (*reinterpret_cast<short*>(uiAllocAdr) == 0x1B72 && *reinterpret_cast<BYTE*>(uiAllocAdr + 2) == 0xA1)
+			{
+				bRetcheck = true;
+				*reinterpret_cast<short*>(uiAllocAdr - 0x6) = 0x21EB; //jmp 0x21 offset
+				memset(reinterpret_cast<void*>(uiAllocAdr - 0x4), 0x90, 4); //nop 4 bytes since we overwrote a cmp
+				i += 0x1C; //retcheck is 0x1C bytes so we don't need to scan those anymore
+			}
+		}
+
+		if (!bRetcheck)
+			VirtualFree(lpAllocation, 0, MEM_RELEASE);
+		else
+			this->m_Address = reinterpret_cast<uint32_t>(lpAllocation);
 	}
 };
--- WinCallingConventionDetection/WinCallingConventionDetection.cpp
+++ WinCallingConventionDetection/WinCallingConventionDetection.cpp
@@ -1,6 +1,46 @@
 #include "UnmanagedPointer.hpp"
 #include "hde32/hde32.h"
 
+namespace Memory
+{
+	bool Compare(const BYTE* pData, const BYTE* bMask, const char* szMask)
+	{
+		for (; *szMask; ++szMask, ++pData, ++bMask)
+			if (*szMask == 'x' && *pData != *bMask) return 0;
+		return (*szMask) == NULL;
+	}
+
+	DWORD FindPattern(DWORD dwAddress, DWORD dwLen, BYTE* bMask, char* szMask)
+	{
+		for (int i = 0; i < (int)dwLen; i++)
+			if (Compare((BYTE*)(dwAddress + (int)i), bMask, szMask))  return (int)(dwAddress + i);
+		return 0;
+	}
+
+	int Scan(DWORD mode, char* content, char* mask, DWORD Offset = 0)
+	{
+		DWORD PageSize;
+		SYSTEM_INFO si;
+		GetNativeSystemInfo(&si);
+		PageSize = si.dwPageSize;
+		MEMORY_BASIC_INFORMATION mi;
+		for (DWORD lpAddr = (DWORD)GetModuleHandleA(0) + Offset; lpAddr < 0x7FFFFFFF; lpAddr += PageSize)
+		{
+			DWORD vq = VirtualQuery((void*)lpAddr, &mi, sizeof(MEMORY_BASIC_INFORMATION));
+			if (vq == ERROR_INVALID_PARAMETER || vq == 0) break;
+			if (mi.Type == MEM_MAPPED) continue;
+			if (mi.Protect == mode)
+			{
+				int addr = FindPattern(lpAddr, PageSize, (PBYTE)content, mask);
+				if (addr != 0)
+				{
+					return addr;
+				}
+			}
+		}
+	}
+}
+
 
 DWORD WINAPI InitializeTest(LPVOID lpThreadParameter)
 {
@@ -17,8 +57,14 @@ DWORD WINAPI InitializeTest(LPVOID lpThreadParameter)
 
 
     auto fixOffset = [](uint32_t address) -> uint32_t { return address - 0x400000 + reinterpret_cast<uint32_t>(GetModuleHandle(nullptr)); };
-    UnmanagedPointer<void()> ok(fixOffset(0x11A6690));
+	DWORD adr = fixOffset(0x1A1F524);
+	DWORD scr = Memory::Scan(PAGE_READWRITE, (char*)&adr, (char*)"xxxx");
+	DWORD ls = scr + 164 + *(DWORD*)(scr + 164);
+	
+    UnmanagedPointer<int(int, const char*, unsigned)> rlua_pushlstring(fixOffset(0x11A63B0));
+	rlua_pushlstring(ls, "str", 3);
 
+	
     getchar();
 	
     return 1;
